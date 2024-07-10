0798316e8e8aece1ffacb8e98b6be8fdddb49db3
DreaMeR <DreaMeR@DREAMER>
2024-07-10 10:47:52 +0600

    Добавьте файлы проекта.

---
+++ Hospital/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Hospital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minForPerson = 10;
+            int personInQueue;
+            int minInHour = 60;
+            int minutes;
+            int hours;
+
+            Console.WriteLine("Добро пожаловать!");
+            Console.WriteLine("Среднее время приема одного человека: " + minForPerson);
+
+            Console.Write("Введите количество человек перед вами: ");
+            personInQueue = Convert.ToInt32(Console.ReadLine());
+
+            hours = (minForPerson * personInQueue) / minInHour;
+            minutes = (minForPerson * personInQueue) % minInHour;
+
+            Console.WriteLine($"Перед вами {personInQueue} человек, время ожидания составит: {hours} часа {minutes} минут");
+        }
+    }
+}
